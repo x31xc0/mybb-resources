0773545c83edcb9802cc6db8227005531a809ae8
スーイツ <Sweets@users.noreply.github.com>
2017-10-08 16:09:34 -0500

    Update mentions.php

--- plugins/mentions.php
+++ plugins/mentions.php
@@ -1,16 +1,13 @@
 <?php
 
-/*
-
-Modified version of Eternal's mentions plugin. Modified because it references parts of MyBB that were modified for Eternal.
-
-   */
-
 if( !defined( "IN_MYBB" ) ) {
     die( "Direct initialization disallowed for the mentions plugin." );
 }
 
-$plugins->add_hook("parse_message", "mentions_change_to_username");
+$plugins->add_hook("parse_message", "replace_uid_mentions");
+$plugins->add_hook("datahandler_post_insert_post", "replace_username_mentions");
+$plugins->add_hook("datahandler_post_insert_thread_post", "replace_username_mentions");
+// TO-DO: PMs, maybe?
 
 function mentions_info() {
     return array(
@@ -19,30 +16,66 @@ function mentions_info() {
         "website" => "https://eternal.is/",
         "author" => "Sweets",
         "authorsite" => "https://eternal.is/user/sweets",
-        "version" => "1.0",
+        "version" => "2.0",
         "guid" => "",
         "compatibility" => "18*"
     );
 }
 
 function stylize($user) {
-	$style = format_name($user['username'], $user['usergroup'], $user['displaygroup']);
-	return "<a href=\"/" . get_profile_link($user['uid']) . "\">" . $style . "</a>";
+    $formatted_name = format_name($user['username'], $user['usergroup'], $user['displaygroup']);
+    return "<a href=\"/" . get_profile_link($user['uid']) . "\">" . $formatted_name . "</a>";
+}
+
+function replace_uid_mentions($message) {
+
+    global $db;
+
+    $uid_regex =  "/\@([\d]+)([^\d]|$)/";
+    preg_match_all($uid_regex, $message, $uid_matches, PREG_SET_ORDER);
+
+    foreach($uid_matches as $match) {
+
+        $query = $db->simple_select("users", "*", "uid = " . $match[1]);
+
+        if ($db->num_rows($query) >= 1) {
+
+            $user = $db->fetch_array($query);
+            $message = str_replace($match[0], stylize($user) . $match[2], $message);
+
+        }
+
+    }
+
+    return $message;
 
 }
 
-function mentions_change_to_username($message) {
-	global $db;
-	$uid_match = "/\@([\d]+)([^\d]|$)/";
-	preg_match_all($uid_match, $message, $uidmatches, PREG_SET_ORDER);
-	foreach($uidmatches as $uid) {
-		$query = $db->simple_select("users", "*", "uid = " . $uid[1]);
-		if ($db->num_rows($query) >= 1) {
-			$user = $db->fetch_array($query);
-			$message = str_replace($uid[0], stylize($user) . $uid[2], $message);
-		}
+function replace_username_mentions(&$post) {
+
+    global $db;
+
+    if ($post->data['savedraft'])
+        return;
+
+    $username_regex = "/\@(\\\\['|\"]{1}|[`]{1})(.+)(\\1)/";
+    preg_match_all($username_regex, $post->post_insert_data['message'], $matches, PREG_SET_ORDER);
+
+    foreach($matches as $match) {
+
+        $query = $db->simple_select("users", "*", "username = \"" . $db->escape_string($match[2]) . "\"");
+
+	if ($db->num_rows($query) >= 1) {
+
+            $uid = $db->fetch_field($query, "uid");
+
+	    if ($uid >= 1)
+                $post->post_insert_data['message'] = str_replace($match[0], "@" . $uid, $post->post_insert_data['message']);
+
 	}
-	return $message;
+
+    }
+
 }
 
 ?>
