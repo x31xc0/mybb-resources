6643a7a2c0647e0feef1d3c77bd9bcb9d020425b
x31xc0 <x31xc0@protonmail.com>
2017-10-14 14:15:52 +0100

    Added function docblocks and cleaned up whitespace

--- plugins/mentions.php
+++ plugins/mentions.php
@@ -1,7 +1,7 @@
 <?php
 
-if( !defined( "IN_MYBB" ) ) {
-    die( "Direct initialization disallowed for the mentions plugin." );
+if (!defined("IN_MYBB")) {
+    die("Direct initialization disallowed for the mentions plugin.");
 }
 
 $plugins->add_hook("parse_message", "replace_uid_mentions");
@@ -9,7 +9,11 @@ $plugins->add_hook("datahandler_post_insert_post", "replace_username_mentions");
 $plugins->add_hook("datahandler_post_insert_thread_post", "replace_username_mentions");
 // TO-DO: PMs, maybe?
 
-function mentions_info() {
+/**
+ * @return array
+ */
+function mentions_info()
+{
     return array(
         "name" => "Mentions",
         "description" => "Mention users by name and uid",
@@ -22,60 +26,60 @@ function mentions_info() {
     );
 }
 
-function stylize($user) {
+/**
+ * @param $user
+ * @return string
+ */
+function stylize($user)
+{
     $formatted_name = format_name($user['username'], $user['usergroup'], $user['displaygroup']);
     return "<a href=\"/" . get_profile_link($user['uid']) . "\">" . $formatted_name . "</a>";
 }
 
-function replace_uid_mentions($message) {
-
+/**
+ * @param $message
+ * @return mixed
+ */
+function replace_uid_mentions($message)
+{
     global $db;
-
-    $uid_regex =  "/\@([\d]+)([^\d]|$)/";
+    $uid_regex = "/\@([\d]+)([^\d]|$)/";
     preg_match_all($uid_regex, $message, $uid_matches, PREG_SET_ORDER);
 
-    foreach($uid_matches as $match) {
-
+    foreach ($uid_matches as $match) {
         $query = $db->simple_select("users", "*", "uid = " . $match[1]);
 
         if ($db->num_rows($query) >= 1) {
-
             $user = $db->fetch_array($query);
             $message = str_replace($match[0], stylize($user) . $match[2], $message);
-
         }
-
     }
 
     return $message;
-
 }
 
-function replace_username_mentions(&$post) {
-
+/**
+ * @param $post
+ */
+function replace_username_mentions(&$post)
+{
     global $db;
 
-    if ($post->data['savedraft'])
+    if ($post->data['savedraft']) {
         return;
+    }
 
     $username_regex = "/\@(\\\\['|\"]{1}|[`]{1})(.+)(\\1)/";
     preg_match_all($username_regex, $post->post_insert_data['message'], $matches, PREG_SET_ORDER);
 
-    foreach($matches as $match) {
-
+    foreach ($matches as $match) {
         $query = $db->simple_select("users", "*", "username = \"" . $db->escape_string($match[2]) . "\"");
 
-	if ($db->num_rows($query) >= 1) {
-
+        if ($db->num_rows($query) >= 1) {
             $uid = $db->fetch_field($query, "uid");
-
-	    if ($uid >= 1)
+            if ($uid >= 1) {
                 $post->post_insert_data['message'] = str_replace($match[0], "@" . $uid, $post->post_insert_data['message']);
-
-	}
-
+            }
+        }
     }
-
 }
-
-?>
